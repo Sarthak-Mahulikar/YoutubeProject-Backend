e68251df1c28974615f71d0de59a4f965d47c3ce
Sarthak-Mahulikar <sarthak.mahulikar@ortigan.in>
2024-05-08 11:47:14 +0530

    added get user channel functionality using mongo pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -285,6 +285,68 @@ const updateAccount = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "account details updated successfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing");
+  }
+  // User.find({username})
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: { $size: "$subscribers" },
+        channelsSubscribedToCount: { $size: "$subscribedTo" },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exist");
+  }
+  console.log(channel);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel fetched sucessfully"));
+});
+
 export {
   registerUser,
   loginUser,
@@ -293,4 +355,5 @@ export {
   changeCurrentPassword,
   getCurrentUser,
   updateAccount,
+  getUserChannelProfile,
 };
